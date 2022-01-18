a6baebb8b44d7a4a0ac0a1a1c1da51e12325fee4
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-01-18 11:40:19 +0800

    feat: 🎸 add directives

---
+++ src/directives/fixed.ts
@@ -0,0 +1,16 @@
+/*
+ * @Description: fixed定位自定义指令
+ * @Author: Xiongjie.Xue(xiongjie.xue@luckincoffee.com)
+ * @Date: 2021-06-23 10:24:44
+ * @LastEditors: Xiongjie.Xue(xxj95719@gmail.com)
+ * @LastEditTime: 2022-01-18 11:28:34
+ */
+const fixedDirective = (el: { style: { [x: string]: string; position: string; }; }, binding: { arg: string; value: string; }) => {
+  el.style.position = 'fixed'
+  const s = binding.arg || 'top'
+  el.style[s] = binding.value + 'px'
+}
+
+export function setupFixedDirective (app: { directive: (arg0: string, arg1: (el: any, binding: any) => void) => void; }) {
+  app.directive('fixed', fixedDirective);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { setupGlobDirectives } from './directives'
+import './style/default.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+setupGlobDirectives(app) // 注册自定义指令
+app.use(router).mount('#app')
