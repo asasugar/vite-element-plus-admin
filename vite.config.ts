918ff47fab58e5ee16c5151f0204ce7398f7a405
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-01-18 11:42:18 +0800

    feat: 🎸 add plugin-proposal-optional-chaining

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,25 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-// import path from "path";
+import { resolve } from 'path';
 
+
+function pathResolve (dir) {
+  return resolve(process.cwd(), '.', dir);
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    // alias: {
-    //   "~/": `${path.resolve(__dirname, "src")}/`,
-    // },
+    alias: [
+      {
+        find: '@',
+        replacement: pathResolve('src') + '/',
+      },
+      {
+        find: '_c',
+        replacement: pathResolve('src/components') + '/',
+      }
+    ]
   },
   plugins: [
     vue(),
