1acd714a8954ec8cf7b1d737b98af4de7b18a8aa
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-01-25 18:32:04 +0800

    fix: 🐛 bug fix

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,7 @@ import 'element-plus/es/components/message/style/css';
 import 'element-plus/es/components/notification/style/css';
 import { registerDynamicRoutes } from '@/router/dynamic';
 mockXHR(); // mock接口
-registerDynamicRoutes(); // 初始化/刷新的时候注册动态路由
+await registerDynamicRoutes(); // 初始化/刷新的时候注册动态路由
 const app = createApp(App);
 setupGlobDirectives(app); // 注册自定义指令
-
 app.use(router).use(store).mount('#app');
