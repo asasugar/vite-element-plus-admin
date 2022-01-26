5eeeb5969952a15339b2570765c47b41b7778ef4
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-01-26 15:38:24 +0800

    feat: 🎸 Clear session on exit

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@
  * @Author: Xiongjie.Xue(xxj95719@gmail.com)
  * @Date: 2022-01-17 20:12:02
  * @LastEditors: Xiongjie.Xue(xxj95719@gmail.com)
- * @LastEditTime: 2022-01-25 14:32:50
+ * @LastEditTime: 2022-01-26 15:36:26
  */
 
 import {
@@ -14,6 +14,7 @@ import {
   RouteRecordRaw
 } from 'vue-router';
 import { clearToken, getToken } from '@/utils/token';
+import { removeSession } from '@/utils/session';
 
 // 基础路由
 const routes: RouteRecordRaw[] = [
@@ -37,8 +38,9 @@ router.beforeEach(
   (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
     document.title = `${to.meta.title} - Vite App`;
     if (to.name !== 'Login' && !getToken()) {
-      // 到登录页，重置token凭证
+      // 到登录页，重置token凭证, 清除session里的动态路由
       clearToken();
+      removeSession('routerMap');
       next({ name: 'Login' });
     } else next();
   }
