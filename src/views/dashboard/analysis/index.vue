4b88a1f8f481ec4abbcb5bcdb0c459572bce8b71
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-02-14 22:47:56 +0800

    refactor: 💡 目录结构重构

--- src/views/dashboard/analysis/index.vue
+++ src/views/dashboard/analysis/index.vue
@@ -3,18 +3,18 @@
  * @Author: Xiongjie.Xue(xxj95719@gmail.com)
  * @Date: 2022-01-21 18:14:27
  * @LastEditors: Xiongjie.Xue(xxj95719@gmail.com)
- * @LastEditTime: 2022-01-26 14:48:21
+ * @LastEditTime: 2022-02-14 22:47:30
 -->
 <template>
   <div class="p20">
-    <GrowCard :loading="loading" :data="data.analysis" />
-    <GrowEChart />
+    <grow-card :loading="loading" :data="data.analysis" />
+    <grow-e-chart />
   </div>
 </template>
 <script lang="ts" setup>
 import { ref, reactive } from 'vue';
-import GrowCard from './components/GrowCard.vue';
-import GrowEChart from './components/GrowEChart.vue';
+import GrowCard from './components/grow-card.vue';
+import GrowEChart from './components/grow-echart.vue';
 
 import { systemService } from '@/services';
 
