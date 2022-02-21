f44e6e45488d121838b2dabb61861ac088bedaca
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-02-21 20:07:23 +0800

    refactor: 💡 use lib fn

--- build/generate/index.ts
+++ build/generate/index.ts
@@ -3,20 +3,22 @@
  * @Author: Xiongjie.Xue(xxj95719@gmail.com)
  * @Date: 2022-02-21 15:23:59
  * @LastEditors: Xiongjie.Xue(xxj95719@gmail.com)
- * @LastEditTime: 2022-02-21 17:11:29
+ * @LastEditTime: 2022-02-21 20:05:03
  */
 import { resolve } from 'path';
 import { readFileSync, writeFileSync } from 'fs';
 import ArtTemplate from 'art-template';
+import { pathResolve } from '../utils';
 
+// Fake Data
 const routes = [{ camelcase: 'Home', path: '@/views/home/index.vue' }];
-// 生成 components.ts
 
+// 生成 auto-import-components.ts
 export function generateDynamicComponents() {
   const indexTpl = readFileSync(resolve(__dirname, './tpls/dynamicComponents.tpl'), 'utf-8');
 
   writeFileSync(
-    resolve(process.cwd(), './src/router/auto-import-components.ts'),
+    pathResolve('src/router/auto-import-components.ts'),
     ArtTemplate.render(indexTpl, {
       data: {
         components: routes
