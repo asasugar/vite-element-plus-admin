d82f8eea03bd16d3c7dbda66c452d41a7609c2a5
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-04-25 14:35:58 +0800

    feat: 🎸 added debounce decorate method

--- src/utils/decorate.ts
+++ src/utils/decorate.ts
@@ -3,12 +3,12 @@
  * @Author: Xiongjie.Xue(xxj95719@gmail.com)
  * @Date: 2022-04-25 14:19:27
  * @LastEditors: Xiongjie.Xue(xxj95719@gmail.com)
- * @LastEditTime: 2022-04-25 14:32:34
+ * @LastEditTime: 2022-04-25 14:34:35
  */
-import { useThrottleFn } from '@vueuse/core';
+import { useThrottleFn, useDebounceFn } from '@vueuse/core';
 
 /**
- * @description: 修饰类方法
+ * @description: 装饰类方法节流
  * @param {*} t
  * @return {*}
  */
@@ -16,5 +16,17 @@ export function throttle(t = 0) {
   return function (_target: any, _methodName: any, desc: any) {
     const oldMethod = desc.value;
     desc.value = useThrottleFn(oldMethod, t);
+  };
+}
+
+/**
+ * @description: 装饰类方法防抖
+ * @param {*} t
+ * @return {*}
+ */
+export function debounce(t = 0) {
+  return function (_target: any, _methodName: any, desc: any) {
+    const oldMethod = desc.value;
+    desc.value = useDebounceFn(oldMethod, t);
   };
 }
