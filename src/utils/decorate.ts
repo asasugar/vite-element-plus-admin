cddbfc4524524ac75abf7336b391e20e327f4eef
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-04-25 14:33:10 +0800

    docs: ✏️ update throttle desc

--- src/utils/decorate.ts
+++ src/utils/decorate.ts
@@ -3,10 +3,15 @@
  * @Author: Xiongjie.Xue(xxj95719@gmail.com)
  * @Date: 2022-04-25 14:19:27
  * @LastEditors: Xiongjie.Xue(xxj95719@gmail.com)
- * @LastEditTime: 2022-04-25 14:22:21
+ * @LastEditTime: 2022-04-25 14:32:34
  */
 import { useThrottleFn } from '@vueuse/core';
 
+/**
+ * @description: 修饰类方法
+ * @param {*} t
+ * @return {*}
+ */
 export function throttle(t = 0) {
   return function (_target: any, _methodName: any, desc: any) {
     const oldMethod = desc.value;
