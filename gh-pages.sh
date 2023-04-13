15463d8caa56f746da6932fc4feea4432a74e321
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2023-04-13 16:13:43 +0800

    ci: 🎡 Update gh-pages.sh

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,11 +3,9 @@
 set -e
 # copy CNAME 404.html到目标文件夹
 cp CNAME 404.html ./dist
-# 进入目标文件夹
-cd dist
-# 提交到本地仓库
-git init
-git add -A
+# dist文件夹添加到暂存区
+git add dist -A
+# 提交说明
 git commit -m 'Updates'
 # 提交到 https://github.com/asasugar/vite-element-plus-admin.git 项目的 vite-element-plus-admin 分支
 git push -f https://github.com/asasugar/vite-element-plus-admin.git master:vite-element-plus-admin
