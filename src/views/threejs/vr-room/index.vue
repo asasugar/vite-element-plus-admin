c3a3cb04c356258a2b8e8ce57e32947273ea738f
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2023-04-14 17:45:11 +0800

    fix: 🐛 Remove $ & add 'github-pages' to eslint ignore

--- src/views/threejs/vr-room/index.vue
+++ src/views/threejs/vr-room/index.vue
@@ -3,7 +3,7 @@
  * @Author: Xiongjie.Xue(xxj95719@gmail.com)
  * @Date: 2022-02-16 17:32:21
  * @LastEditors: Xiongjie.Xue(xxj95719@gmail.com)
- * @LastEditTime: 2023-01-10 11:04:09
+ * @LastEditTime: 2023-04-13 18:44:34
 -->
 <template>
   <div id="VR" ref="vrRoomRef"></div>
@@ -11,10 +11,10 @@
 <script lang="ts" setup>
 import { onMounted } from 'vue';
 import VrRoom from './index';
-const vrRoomRef = $shallowRef<HTMLElement | undefined>(document.documentElement);
+const vrRoomRef = shallowRef<HTMLElement | undefined>(document.documentElement);
 
 onMounted(() => {
-  new VrRoom('#VR', vrRoomRef);
+  new VrRoom('#VR', vrRoomRef.value);
 });
 </script>
 <style lang="less" scoped></style>
